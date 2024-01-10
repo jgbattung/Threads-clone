33393522bdfb74c72b3b54b57e1b04115827503b
Jireh Battung <jirehgbattung@gmail.com>
2024-01-10 19:51:24 +0800

    move sidebarRoutes

---
+++ constants/index.js
@@ -0,0 +1,27 @@
+export const sidebarRoutes = [
+  {
+    route: "/",
+    image: "/assets/home.svg",
+    text: "Home"
+  },
+  {
+    route: "/search",
+    image: "/assets/search.svg",
+    text: "Search"
+  },
+  {
+    route: "/activity",
+    image: "/assets/activity.svg",
+    text: "Activity"
+  },
+  {
+    route: "/home",
+    image: "/assets/create.svg",
+    text: "Create Thread"
+  },
+  {
+    route: "/profile",
+    image: "/assets/profile.svg",
+    text: "Profile"
+  },
+]
