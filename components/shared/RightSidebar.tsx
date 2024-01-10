dfe66b3ed0d94d5254fbad813bee0fac4b087c7f
Jireh Battung <jirehgbattung@gmail.com>
2024-01-10 20:13:17 +0800

    add rightsidebar

--- components/shared/RightSidebar.tsx
+++ components/shared/RightSidebar.tsx
@@ -1,5 +1,3 @@
-import React from 'react'
-
 function RightSidebar() {
   return (
     <section className='sticky right-0 top-0 z-20 px-8 flex flex-col h-screen w-fit justify-start overflow-auto border-l border-l-gray-950 bg-gray-900 pt-28 max-md:hidden'>
