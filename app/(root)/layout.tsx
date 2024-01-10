f76a2eb6516b03f01e48de2ec62fe9815c436ea7
Jireh Battung <jirehgbattung@gmail.com>
2024-01-10 19:52:12 +0800

    update home layout

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -2,6 +2,9 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import '../globals.css'
 import { ClerkProvider } from '@clerk/nextjs'
+import Navbar from '@/components/shared/Navbar'
+import LeftSidebar from '@/components/shared/LeftSidebar'
+import Footer from '@/components/shared/Footer'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -18,7 +21,20 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={inter.className}>{children}</body>
+        <body className={inter.className}>
+          <Navbar />
+
+          <main className="flex flex-row">
+            <LeftSidebar />
+            <section className='flex min-h-screen flex-1 flex-col items-center bg-gray-950 px-6 pb-10 pt-28 max-md:pb-32 sm:px-10'>
+              <div className='w-full max-w-4xl'>
+                {children}
+              </div>
+            </section>
+            {/* <RightSidebar /> */}
+          </main>
+          <Footer />
+        </body>
       </html>
     </ClerkProvider>
   )
