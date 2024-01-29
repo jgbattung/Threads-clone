eecc11eb74f7814efb9a6610766affeed76d99aa
Jireh Battung <jirehgbattung@gmail.com>
2024-01-29 17:35:49 +0800

    reply form and page

--- app/(root)/thread/[id]/page.tsx
+++ app/(root)/thread/[id]/page.tsx
@@ -1,4 +1,5 @@
 import ThreadCard from "@/components/cards/ThreadCard";
+import Reply from "@/components/forms/Reply";
 import { fetchThreadById } from "@/lib/actions/thread.actions";
 import { fetchUser } from "@/lib/actions/user.actions";
 import { currentUser } from "@clerk/nextjs";
@@ -28,6 +29,30 @@ const Page = async ({ params }: { params: { id: string } }) => {
           createdAt={thread.createdAt}
           comments={thread.children}
         />
+      </div>
+
+      <div className="mt-7">
+        <Reply 
+          threadId={thread.id}
+          currentUserImg={userInfo.image}
+          currentUserId={JSON.stringify(userInfo._id)}
+        />
+      </div>
+      
+      <div className="mt-10">
+        {thread.children.map((child: any) => (
+          <ThreadCard 
+            key={child._id}
+            id={child._id}
+            currentUser={user?.id || ""}
+            parentId={child.parentId}
+            content={child.text}
+            author={child.author}
+            createdAt={child.createdAt}
+            comments={child.children}
+            isComment
+          />
+        ))}
       </div>
     </section>
   )
