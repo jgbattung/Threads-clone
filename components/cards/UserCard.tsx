d5e8f9d03afc0929cf6fe2f04d53120445696725
Jireh Battung <jirehgbattung@gmail.com>
2024-01-31 17:48:52 +0800

    create user card

---
+++ components/cards/UserCard.tsx
@@ -0,0 +1,38 @@
+import Image from "next/image";
+
+interface Props {
+  id: string;
+  name: string;
+  username: string;
+  imgUrl: string;
+  bio: string;
+}
+
+const UserCard = ({
+  id,
+  name,
+  username,
+  imgUrl,
+  bio
+}: Props) => {
+  return (
+    <article className="flex flex-row justify-between gap-4 max-sm:rounded-xl max-sm:bg-gray-950 sm:flex-row sm:items-center">
+      <div className="flex flex-1 items-start justify-start gap-3 sm:items-start">
+        <Image 
+          src={imgUrl}
+          alt="profile photo"
+          width={48}
+          height={48}
+          className="rounded-full object-cover h-12 w-12"
+        />
+        <div className="flex-1 text-ellipsis">
+          <h4 className="font-bold text-lg text-gray-200">{name}</h4>
+          <p className="font-light text-sm text-gray-400">@{username}</p>
+          <p className="text-gray-200 font-normal text-base">{bio}</p>
+        </div>
+      </div>
+    </article>
+  )
+};
+
+export default UserCard
